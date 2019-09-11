f1526756ad748b64162a616ce6c2c96cce19ed44
Svenn Kristoffer Berg <32140076+skberg@users.noreply.github.com>
2019-09-11 15:21:53 +0200

    update

---
+++ build/precache-manifest.00213ae34d00d8347e8fb0ffe5b46793.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a15c1c232bec9ea6774",
+    "url": "/static/js/main.b1e0448e.chunk.js"
+  },
+  {
+    "revision": "92ecc12e2ae46cbe9112",
+    "url": "/static/js/2.ba329198.chunk.js"
+  },
+  {
+    "revision": "0a15c1c232bec9ea6774",
+    "url": "/static/css/main.377ff90e.chunk.css"
+  },
+  {
+    "revision": "92ecc12e2ae46cbe9112",
+    "url": "/static/css/2.4c811e26.chunk.css"
+  },
+  {
+    "revision": "de5be6346ca6edc752267f1884f4275d",
+    "url": "/index.html"
+  }
+];
